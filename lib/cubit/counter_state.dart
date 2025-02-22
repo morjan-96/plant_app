8c9dd9b715f6b61711871c65893a0df87ec420f8
morjan <116336380+morjan-96@users.noreply.github.com>
2025-02-22 06:11:02 +0200

    22 feb

--- lib/cubit/counter_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'counter_cubit.dart';
-
-final class CounterState {
-  int counterValue;
-  bool wasIncrement;
-
-  CounterState({required this.counterValue, required this.wasIncrement});
-}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,26 +20,7 @@ class _HomePageState extends State<HomePage> {
       ),
       body: const Padding(
         padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text(
-              'WELCOME BACK ...',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'The RichText widget displays text that uses multiple different styles. The text to display is described using a tree of TextSpan objects, each of which has an associated style that is used for that subtree. The text might break across multiple lines or might all be displayed on the same line depending on the layout constraints.',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(),
-            SizedBox(height: 10),
-            TextField(),
-            SizedBox(height: 10),
-          ],
-        ),
+        child: 
       ),
     );
   }
